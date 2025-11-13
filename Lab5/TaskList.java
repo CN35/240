d70641a80c3385e4981f4b5d19f49bfbd43957a7
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-13 11:28:00 -0600

    Update TaskList.java

--- Lab5/TaskList.java
+++ Lab5/TaskList.java
@@ -5,12 +5,18 @@ public class TaskList {
         System.out.println("Priority Task List\n------------------");
         Task task1,task2,task3,task4,task5 = {task, id};//for(int i=0;i<5;i++)
         //String space="";
-        //{// for(){=space.conc}
-          task1.getPrirority(); task1.setPrirority();
-          task2.getPrirority(); task2.setPrirority();
-          task3.getPrirority(); task3.setPrirority();
-          task4.getPrirority(); task4.setPrirority();
-          task5.getPrirority(); task5.setPrirority();
-          System.out.println("\t\t"+/*+(-.length+2)*/);}
+        // for(){=space.conc}
+        task1.getPrirority(); task1.setPrirority();
+        task2.getPrirority(); task2.setPrirority();
+        task3.getPrirority(); task3.setPrirority();
+        task4.getPrirority(); task4.setPrirority();
+        task5.getPrirority(); task5.setPrirority();
+  
+  // {  
+        /*  */System.out.println("\t\t"+/*+(-.length+2)*/);}
+        /*  */System.out.println("\t\t"+/*+(-.length+2)*/);}
+        /*  */System.out.println("\t\t"+/*+(-.length+2)*/);}
+        /*  */System.out.println("\t\t"+/*+(-.length+2)*/);}
+        /*  */System.out.println("\t\t"+/*+(-.length+2)*/);}
   }
 }
