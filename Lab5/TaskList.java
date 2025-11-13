530c84ef2acd36029e056cab85f5c589eee8f5e4
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-13 11:21:47 -0600

    Update TaskList.java

--- Lab5/TaskList.java
+++ Lab5/TaskList.java
@@ -3,7 +3,7 @@ public class TaskList {
     public static void main(String[] args) {
         //inv Task
         System.out.println("Priority Task List\n------------------");
-        String task1,task2,task3,task4,task5;for()
+        Task task1,task2,task3,task4,task5 = {task, id};for(int i=0;i<5;i++)
         //String space="";
         {// for(){=space.conc}
           System.out.println("\t\t"+/*+(-.length+2)*/);}
