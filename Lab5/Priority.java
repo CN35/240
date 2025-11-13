c6fb1b223e68680773ffe92646b2c2bfa8a7715d
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-13 11:34:19 -0600

    Update Priority.java

--- Lab5/Priority.java
+++ Lab5/Priority.java
@@ -1,6 +1,4 @@
-public class Prirority {
-    public static void main(String[] args) {
-      public void setPrirority() {}
-      public void getPrirority() {}
-  }
+public interface Prirority {
+    void setPrirority();
+    void getPrirority();
 }
