8fd7327fc84e55407c0499410a7a97512cfd523a
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-20 14:25:59 -0600

    Update Task.java

--- Lab5/Task.java
+++ Lab5/Task.java
@@ -1,12 +1,12 @@
 public class Task implements Prirority{
 
-private String name;
+public String name;
 private int id=-1;
 
   public Task(String name){this.name=name;}
   public Task(int id){this.id=id;}
   
-    public void setPrirority(id) {Task.id=id;}
+    public void setPrirority(int id) {Task.id=id;}
     public int getPrirority() {return task.id;}
 }
 
