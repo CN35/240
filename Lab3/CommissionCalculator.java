19693f3d6f1b9c2f228937e64a7f162c21b2f128
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-20 10:35:53 -0600

    Update CommissionCalculator.java

--- Lab3/CommissionCalculator.java
+++ Lab3/CommissionCalculator.java
@@ -1,47 +1,101 @@
-import java.util.*;
+import java.text.NumberFormat;
 
-public class CommissionCalculator {public void run(){
-       // Create and set an Auto policy object setting data using constructor
-       Auto a = new Auto("Jerry", "Seinfed", "Chevy", "Malibu", 10000.0, 50000.0);
+public class Auto {
 
-       // Compute commission
-       a.computeCommission(/*name4,make4,model4,liability4,collision,commission4*/);
-  
-       // Print auto policy
-       System.out.println(a);
-      
-       // Create and set a Home policy object setting data using setters
-       Home h = new Home("Elaine", "Benes", 5000, 4000, 32000, 10000);
+	// Class fields
+	private final double PERCENT = 0.3;
+	private String firstName, lastName;
+	private String make;
+	private String model;
+	private double liability;
+	private double collision;
+	private double commission;
+	protected NumberFormat fmt;
+	
+	// Class methods
+	// Constructor
+	public Auto()
+	{
+		firstName = null;
+		lastName = null;
+		make = null;
+		model = null;
+		liability = 0.0;
+		collision = 0.0;
+		commission = 0.0;
+		fmt = NumberFormat.getCurrencyInstance();
+	}
+	public Auto(String firstName, String lastName, String make, String model, double liability, double collision)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.make = make;
+		this.model = model;
+		this.liability = liability;
+		this.collision = collision;
+		fmt = NumberFormat.getCurrencyInstance();
+	}
+	
+	// Compute commission
+	public void computeCommission() {
+		commission = (liability + collision) * PERCENT;
+	}
 
-       h.setFirstName("Elaine");
-       h.setLastName("Benes");
-       h.setContents(5000);//are these nessiacary
-       h.setDwelling(32000);
-       h.setFootage(4000);//What this is for
-       h.setLiability(10000);
+	// toString overridden from Object class
+	@Override
+	public String toString() {
+		String result = "Auto Policy" + '\n';
+		result += "-----------" + '\n';
+		result += "Name: " + firstName + ' ' + lastName + '\n';
+		result += "Make: " + make + '\n';
+		result += "Model: " + model + '\n';
+		result += "Liability: " + fmt.format(liability) + '\n';
+		result += "Collision: " + fmt.format(collision) + '\n';
+		result += "Commission: " + fmt.format(commission) + '\n';
+		return result;
+	}
 
-       // Compute commission
-       h.computeCommission();
-
-       // Print home policy
-       System.out.println(h);
-
-       // Create and set an Life policy object setting data using constructor
-       Life l = new Life("Cosmo", "Kramer", 35, 50000);
-
-       // Compute commission
-       l.computeCommission();
-
-       // Print life policy
-       System.out.println(l);
-
-       // Test getters
-       System.out.println("Get life firstName: " + l.getFirstName());
-       System.out.println("Get life lastName: " + l.getLastName());
-       System.out.println("Get life age: " + l.getAge());
-       System.out.println("Get life term: " + l.getTerm());
-    
-    }
-    
-    public static void main(String[] args){CommissionCalculator calc = new CommissionCalculator();calc.run();}
+	// Getters and setters
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public double getLiability() {
+		return liability;
+	}
+	public void setLiability(double liability) {
+		this.liability = liability;
+	}
+	public double getCollision() {
+		return collision;
+	}
+	public void setCollision(double collision) {
+		this.collision = collision;
+	}
+	public double getCommission() {
+		return commission;
+	}
+	public void setCommission(double commission) {
+		this.commission = commission;
+	}
 }
