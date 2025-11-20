babf15e4c681250b4934a73a2ca0cade067576fb
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-20 11:07:38 -0600

    Update Task.java

--- Lab5/Task.java
+++ Lab5/Task.java
@@ -3,6 +3,7 @@ public class Task {
 private String name;
 private int id=-1;
 
-  public Task(String name){this.name=name}
-  public Task(int id){this.id=id}
+  public Task(String name){this.name=name;}
+  public Task(int id){this.id=id;}
 }
+
