a8a7b805ff825f4c89aa255e55fac9021570d0e7
CN35 <104024159+CN35@users.noreply.github.com>
2025-11-20 15:29:26 -0600

    Update Task.java

--- Lab5/Task.java
+++ Lab5/Task.java
@@ -6,7 +6,6 @@ private int id=-1;
   public Task(String name){this.name=name;}
   public Task(int id){this.id=id;}
   
-    public void setPrirority(int id) {id=id;}//rename one of the two id variabl
+    public void setPrirority(int id) {id=id;}//maybe I rename one of the two id variabl
     public int getPrirority() {return id;}
 }
-
