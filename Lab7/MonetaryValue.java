fd66ed4271abf6a5e94ed359565b3ef89c824d11
CN35 <104024159+CN35@users.noreply.github.com>
2025-12-08 13:54:36 -0600

    Update MonetaryValue.java

--- Lab7/MonetaryValue.java
+++ Lab7/MonetaryValue.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;public class MonetaryValue {
-
+  import Math.random;
     sc = new Scanner(System.in);String ch = sc.nextLine();
     while (ch!="quit")
-    {}
-
+    {
+    System.println(Math.random()+"ten dollar bills");
+    System.println(Math.random()+"five dollar bills");
+    System.println(Math.random()+"one dollar bills");
+    System.println(Math.random()+"quarters");
+    System.println(Math.random()+"dimes");
+    System.println(Math.random()+"nickels");
+    System.println(Math.random()+"pennies");
+    }
+System.println("EXCEPTION: Invalid input");
 
 }
