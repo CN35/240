43f1fd4d5b100c7d6c952f3a31eeae39f5c364d0
CN35 <104024159+CN35@users.noreply.github.com>
2025-12-08 14:07:58 -0600

    Update Gradebook.java

--- Lab6/Gradebook.java
+++ Lab6/Gradebook.java
@@ -1,3 +1,10 @@
-    // public static (){}
+import java.util.Math;
+public class NewClass1 {
+
+  //First names
+  //Last names
     for (int t = 1;t=5;t++)
-    {}
+    {System.println();}
+  }
+
+}
