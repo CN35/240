b1be9acea365fd8602e9de355e84984a3690a334
CN35 <104024159+CN35@users.noreply.github.com>
2025-12-08 17:47:40 -0600

    Update RationalNumber.java

--- Lab8/RationalNumber.java
+++ Lab8/RationalNumber.java
@@ -1,7 +1,11 @@
 import java.lang.Math/***/;
-
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello, World!");
-  }
+public class Main {public static void main(String[] args) {
+   
+   
+   
+      ;
+      
+      
+      
+}
 }
