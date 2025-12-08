e20d5626a4e9b3d848f591759dc2e9d7f5a01435
CN35 <104024159+CN35@users.noreply.github.com>
2025-12-07 19:14:05 -0600

    Update and rename Priority.java to Comparable.java

--- Lab6/Comparable.java
+++ Lab6/Comparable.java
@@ -1,11 +1,11 @@
 public interface Priority 
 {
 	// Constants for max and min priority levels
-	static final int MAX_PRIORITY = 1;
-	static final int MIN_PRIORITY = 5;
+	static final <identifier> <name> = <value>;
+	static final <identifier> <name> = <value>;
 
 	// Abstract method that sets an object's priority level
-	public void setPriority(int value);
+	public void /*function here*/;
 
 	// Abstract method that returns an object's priority level
 	public int getPriority();
