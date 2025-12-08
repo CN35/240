2e74411f39114cd76a1d16703a2b24968db67df8
CN35 <104024159+CN35@users.noreply.github.com>
2025-12-08 14:09:27 -0600

    Update Gradebook.java

--- Lab6/Gradebook.java
+++ Lab6/Gradebook.java
@@ -1,9 +1,7 @@
 import java.util.Math;
 public class NewClass1 {
 
-  //First names
-  //Last names
-    for (int t = 1;t=5;t++)
+    for (int t = 1;t=5;t++)//make sure to delete whatever first and last name is randomly selected so that it doesn't choose multiple of the same
     {System.println();}
   }
 
